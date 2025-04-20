fabcf678acdd740d2e1627391e0355202161cdf7
wichayarin444 <6531501183@lamduan.mfu.ac.th>
2025-04-20 16:39:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ with open('kmeans_model.pkl', 'rb') as f:
 
 # Page config
 st.set_page_config(page_title="k-Means Clustering App", layout="centered")
-st.title("🔍 k-Means Clustering Visualizer")
+st.title("🔍 k-Means Clustering Visualizer by Wichayarin Rattaphonwalitpong")
 st.subheader("📊 Example Data for Visualization")
 st.markdown("This demo uses example data (2D) to illustrate clustering results.")
 
